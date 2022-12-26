e618ab501d41c97f65b317fdc332afa0ea17298b
Chrizhuu <chriszhu@rct.ai>
2022-12-25 19:14:20 -0500

    Color Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      "Black":"#010800",
+      "Seal Brown":"#6B3419",
+      "Russet":"#824B18",
+      "Gold":"#B17715",
+      "Maximum Green Yellow":"#C8E747",
+      "Dark Moss Green":"#365208",
+      "Lincoln Green":"#224208",
+    },
   },
   plugins: [],
 }
